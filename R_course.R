c15724a3ea0fec145737d0bb1fe86793a7ee2fb7
JUAN AGUINAGA <juanpaguinaga@gmail.com>
2022-09-30 18:44:07 +0200

    First commit

--- R_course.R
+++ R_course.R
@@ -1,2 +1,18 @@
 #R_course
 
+#seccion 2
+
+data(cars)
+head(cars)
+
+plot(x=cars$speed, y=cars$dist)
+cor.test(cars$speed, cars$dist)# pearson correlation
+cor.test(cars$speed, cars$dist, method = "spearman", exact = FALSE)
+cor.test(cars$speed, cars$dist, method = "kendal", exact = FALSE)
+
+library(ppcor)
+data(Cars93)
+#Min.Price, MPG.city, MPG.highway
+pcor.test(Cars93$MPG.city, Cars93$MPG.highway, Cars93$Min.Price, method = "pearson")
+
+plot(Cars93$MPG.city, Cars93$Min.Price)
